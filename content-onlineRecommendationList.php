6a5400325329c01dc9975cc41b1abd5b3e0f9d2b
Pavel Pestau <pestovpvl@mail.ru>
2019-06-25 14:46:39 -0400

    Was added two page

---
+++ content-onlineRecommendationList.php
@@ -0,0 +1,79 @@
+<div class="content-2 onlineRecommendationList-wraper">
+    <div class="content-box">
+        <div class="row">
+            <div class="col-lg-12">
+                <div style="max-height: 50px;" class="element-wrapper">
+                    <h6 class="element-header"> Online recommendation list</h6>
+                </div>
+                <div class="text__descriptyion_wrap mt-2">
+                    <p>Account No: <span>101</span></p>
+
+                    <p>
+                        Account Name: <span>Shavas Aniyim</span> </p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="element-wrapper">
+            <div class="element-box">
+                <div class="row">
+                    <div class="col-sm-12">
+                    <div class="element-wrapper">
+  <div class="element-box">
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr class="webgrid-header">
+                                        <th scope="col">
+                                            For Organization </th>
+                                        <th scope="col">
+                                            Amount </th>
+                                        <th scope="col">
+                                            Recom. On </th>
+                                        <th scope="col">
+                                            Mailed To </th>
+                                        <th scope="col">
+                                            Cancel? </th>
+                                        <th scope="col">
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="webgrid-row-style">
+                                        <td class="col2Width">The Ojc Fund</td>
+                                        <td class="col1Width">$0.01</td>
+                                        <td class="col1Width">3/28/2019</td>
+                                        <td class="col2Width"></td>
+                                        <td>
+                                            <a class="mr-2 mb-2 btn btn-outline-danger"
+                                                href="/Account/CancelRecommendation?voucherId=MwAAADIAAAA2AAAANgAAADMAAAA5AAAAOAAAAA%3D%3D">Cancel</a>
+                                        </td>
+                                        <td>
+                                            <a class="mr-2 mb-2 btn btn-outline-primary"
+                                                href="/Account/Recommendation?recurring=False&amp;voucherId=MwAAADIAAAA2AAAANgAAADMAAAA5AAAAOAAAAA%3D%3D">Repeat</a>
+                                        </td>
+                                    </tr>
+                                    <tr class="webgrid-alternating-row">
+                                        <td class="col2Width">A F O Shehebar Sephardic Center Incorporated</td>
+                                        <td class="col1Width">$3.75</td>
+                                        <td class="col1Width">8/8/2017</td>
+                                        <td class="col2Width">34 West 33Rd St. <br>New York , NY 10001</td>
+                                        <td>
+                                            <a class="mr-2 mb-2 btn btn-outline-secondary">Processed</a>
+                                        </td>
+                                        <td>
+                                            <a class="mr-2 mb-2 btn btn-outline-primary"
+                                                href="/Account/Recommendation?recurring=False&amp;voucherId=MQAAADMAAAA0AAAAOAAAADEAAAAyAAAANQAAAA%3D%3D">Repeat</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        </div>
+                        </div>
+                        <a class="btn btn-primary" href="/Account/Summary">Account Summary</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ content-recurrRecommendationList.php
@@ -0,0 +1,249 @@
+<div class="content-2 onlineRecommendationList-wraper">
+    <div class="content-box">
+        <div class="row">
+            <div class="col-lg-12">
+                <div style="max-height: 50px;" class="element-wrapper">
+                    <h6 class="element-header"> Monthly Recommendation List</h6>
+                </div>
+                <div class="text__descriptyion_wrap mt-2">
+                    <p>Account No: <span>101</span></p>
+
+                    <p>
+                        Account Name: <span>Shavas Aniyim</span> </p>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="element-wrapper">
+            <div class="element-box">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="element-wrapper">
+                            <div class="element-box">
+                                <div class="table-responsive">
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr class="webgrid-header">
+                                                <th scope="col">
+                                                    For Organization </th>
+                                                <th scope="col">
+                                                    Recommendation Amount </th>
+                                                <th scope="col">
+                                                    On Day of Month </th>
+                                                <th scope="col">
+                                                    Months scheduled </th>
+                                                <th scope="col">
+                                                    Months to recurr </th>
+                                                <th scope="col">
+                                                    Starting From </th>
+                                                <th scope="col">
+                                                </th>
+                                                <th scope="col">
+                                                </th>
+                                                <th scope="col">
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr class="webgrid-row-style">
+                                                <td>Kids Of Courage Inc </td>
+                                                <td class="col1Width">$50.00</td>
+                                                <td class="col1Width">5</td>
+                                                <td class="col1Width">12</td>
+                                                <td class="col1Width">12</td>
+                                                <td class="col1Width">06/12/2019</td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-secondary"
+                                                        href="/Account/UpdateRecommendation?RecurrRecoID=MgAAADAAAAAwAAAANwAAAA%3D%3D">Change</a>
+                                                </td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-primary"
+                                                        href="/Account/PauseRecurrRecommendation?RecurrRecoID=MgAAADAAAAAwAAAANwAAAA%3D%3D&amp;pause=True">Pause</a>
+                                                </td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-danger"
+                                                        href="/Account/CancelRecurringRecommendation?RecurrRecoID=MgAAADAAAAAwAAAANwAAAA%3D%3D">Cancel</a>
+                                                </td>
+                                            </tr>
+                                            <tr class="webgrid-alternating-row">
+                                                <td>The Ojc Fund</td>
+                                                <td class="col1Width">$56.00</td>
+                                                <td class="col1Width">18</td>
+                                                <td class="col1Width">1</td>
+                                                <td class="col1Width">1</td>
+                                                <td class="col1Width">07/01/2019</td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-secondary"
+                                                        href="/Account/UpdateRecommendation?RecurrRecoID=MQAAADYAAAA3AAAAOAAAAA%3D%3D">Change</a>
+                                                </td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-primary"
+                                                        href="/Account/PauseRecurrRecommendation?RecurrRecoID=MQAAADYAAAA3AAAAOAAAAA%3D%3D&amp;pause=True">Pause</a>
+                                                </td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-danger"
+                                                        href="/Account/CancelRecurringRecommendation?RecurrRecoID=MQAAADYAAAA3AAAAOAAAAA%3D%3D">Cancel</a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="element-wrapper">
+                            <div class="element-box">
+                                <h5 class="form-header">
+                                    Recurring recommendations set by charities
+                                </h5>
+
+                                <div class="table-responsive">
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr class="webgrid-header">
+                                                <th scope="col">
+                                                    For Organization </th>
+                                                <th scope="col">
+                                                    Recommendation Amount </th>
+                                                <th scope="col">
+                                                    On Day of Month </th>
+                                                <th scope="col">
+                                                    Months scheduled </th>
+                                                <th scope="col">
+                                                    Months to recurr </th>
+                                                <th scope="col">
+                                                    Starting From </th>
+                                                <th scope="col">
+                                                    Charity Card </th>
+                                                <th scope="col">
+                                                </th>
+                                                <th scope="col">
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr class="webgrid-row-style">
+                                                <td>The Ojc Fund</td>
+                                                <td class="col1Width">$5.00</td>
+                                                <td class="col1Width">5</td>
+                                                <td class="col1Width">5</td>
+                                                <td class="col1Width">1</td>
+                                                <td class="col1Width">02/28/2019</td>
+                                                <td>****2419</td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-primary"
+                                                        href="/Account/PauseRecurrRecommendation?RecurrRecoID=MQAAADcAAAA5AAAAOQAAAA%3D%3D&amp;pause=True">Pause</a>
+                                                </td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-danger"
+                                                        href="/Account/CancelRecurringRecommendation?RecurrRecoID=MQAAADcAAAA5AAAAOQAAAA%3D%3D">Cancel</a>
+                                                </td>
+                                            </tr>
+                                            <tr class="webgrid-alternating-row">
+                                                <td>The Ojc Fund</td>
+                                                <td class="col1Width">$5.00</td>
+                                                <td class="col1Width">15</td>
+                                                <td class="col1Width">5</td>
+                                                <td class="col1Width">1</td>
+                                                <td class="col1Width">02/28/2019</td>
+                                                <td>****2419</td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-primary"
+                                                        href="/Account/PauseRecurrRecommendation?RecurrRecoID=MQAAADcAAAA5AAAAOAAAAA%3D%3D&amp;pause=True">Pause</a>
+                                                </td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-danger"
+                                                        href="/Account/CancelRecurringRecommendation?RecurrRecoID=MQAAADcAAAA5AAAAOAAAAA%3D%3D">Cancel</a>
+                                                </td>
+                                            </tr>
+                                            <tr class="webgrid-row-style">
+                                                <td>The Ojc Fund</td>
+                                                <td class="col1Width">$5.00</td>
+                                                <td class="col1Width">31</td>
+                                                <td class="col1Width">1</td>
+                                                <td class="col1Width">1</td>
+                                                <td class="col1Width">09/01/2019</td>
+                                                <td>****2419</td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-info"
+                                                        href="/Account/PauseRecurrRecommendation?RecurrRecoID=MQAAADYAAAA4AAAANQAAAA%3D%3D&amp;pause=False">Activate</a>
+                                                </td>
+                                                <td><a class="mr-2 mb-2 btn btn-outline-danger"
+                                                        href="/Account/CancelRecurringRecommendation?RecurrRecoID=MQAAADYAAAA4AAAANQAAAA%3D%3D">Cancel</a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="element-wrapper">
+                            <div class="element-box">
+                                <h5 class="form-header">
+                                    Your total running recurrings by month
+                                </h5>
+
+                                <div class="table-responsive">
+
+
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr class="webgrid-header">
+                                                <th scope="col">
+                                                    Month </th>
+                                                <th scope="col">
+                                                    Amount </th>
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr class="webgrid-footer">
+                                                <td colspan="2">1 <a
+                                                        href="/Account/RecurrRecommendationList?page=2">2</a> <a
+                                                        href="/Account/RecurrRecommendationList?page=2">&gt;</a> </td>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            <tr class="webgrid-row-style">
+                                                <td>Jul 2019</td>
+                                                <td>$116.00</td>
+                                            </tr>
+                                            <tr class="webgrid-alternating-row">
+                                                <td>Aug 2019</td>
+                                                <td>$50.00</td>
+                                            </tr>
+                                            <tr class="webgrid-row-style">
+                                                <td>Sep 2019</td>
+                                                <td>$50.00</td>
+                                            </tr>
+                                            <tr class="webgrid-alternating-row">
+                                                <td>Oct 2019</td>
+                                                <td>$50.00</td>
+                                            </tr>
+                                            <tr class="webgrid-row-style">
+                                                <td>Nov 2019</td>
+                                                <td>$50.00</td>
+                                            </tr>
+                                            <tr class="webgrid-alternating-row">
+                                                <td>Dec 2019</td>
+                                                <td>$50.00</td>
+                                            </tr>
+                                            <tr class="webgrid-row-style">
+                                                <td>Jan 2020</td>
+                                                <td>$50.00</td>
+                                            </tr>
+                                            <tr class="webgrid-alternating-row">
+                                                <td>Feb 2020</td>
+                                                <td>$50.00</td>
+                                            </tr>
+                                            <tr class="webgrid-row-style">
+                                                <td>Mar 2020</td>
+                                                <td>$50.00</td>
+                                            </tr>
+                                            <tr class="webgrid-alternating-row">
+                                                <td>Apr 2020</td>
+                                                <td>$50.00</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+
+
+                                </div>
+                            </div>
+                        </div>
+                       
+                        <a class="btn btn-primary" href="Recommendation?recurring=true">Schedule a new <strong>Monthly Recommendation</strong></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
